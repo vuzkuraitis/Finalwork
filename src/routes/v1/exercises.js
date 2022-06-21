2bcdb0b06b1fa44cc37cf12897e2cc555c6ee7e9
Vidmantas Uzkuraitis <v.uzkuraitis@yahoo.com>
2022-06-21 19:08:05 +0300

    added exercises post

--- src/routes/v1/exercises.js
+++ src/routes/v1/exercises.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
+const Joi = require('joi');
 
 const { mysqlConfig } = require('../../config');
 const isLoggedIn = require('../../middleware/auth');
+const validation = require('../../middleware/validation');
 
 const router = express.Router();
+
+const exerciseSchema = Joi.object({
+  name: Joi.string().lowercase().required(),
+  description: Joi.string().lowercase().required(),
+});
 
 router.get('/', isLoggedIn, async (req, res) => {
   try {
@@ -13,6 +20,23 @@ router.get('/', isLoggedIn, async (req, res) => {
     await con.end();
 
     return res.send(data);
+  } catch (err) {
+    return res.status(500).send({ err: 'Server issue occured. Please try again later' });
+  }
+});
+
+router.post('/', isLoggedIn, validation(exerciseSchema), async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const [data] = await con.execute(`
+      INSERT INTO exercises (name, description)
+      VALUES (${mysql.escape(req.body.name)}, ${mysql.escape(req.body.description)})`);
+    await con.end();
+
+    if (!data.insertId) {
+      return res.status(500).send({ err: 'Please try again' });
+    }
+    return res.send({ msg: 'Successfully added an Exercise' });
   } catch (err) {
     return res.status(500).send({ err: 'Server issue occured. Please try again later' });
   }
