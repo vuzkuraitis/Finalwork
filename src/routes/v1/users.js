ac197bdbc7780a2ad10f225f4ce3a2d4fb5b2ef2
Vidmantas Uzkuraitis <v.uzkuraitis@yahoo.com>
2022-07-18 23:35:11 +0300

    fix

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -51,18 +51,6 @@ router.get('/', isLoggedIn, async (req, res) => {
   }
 });
 
-router.get('/gettokens', async (req, res) => {
-  try {
-    const con = await mysql.createConnection(mysqlConfig);
-    const [data] = await con.execute(`SELECT * FROM reset_tokens WHERE email = ${req.body.email}`);
-    await con.end();
-
-    return res.send(data);
-  } catch (err) {
-    return res.status(500).send({ err: 'Server issue occured. Please try again later' });
-  }
-});
-
 router.post('/register', validation(registrationSchema), async (req, res) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 10);
@@ -184,6 +172,7 @@ router.post('/reset-password', validation(resetPasswordSchema), async (req, res)
     }
     return res.send({ msg: 'If your email is correct you will shortly get a message' });
   } catch (err) {
+    console.log(err);
     return res.status(500).send({ err: 'Server issue occured. Please try again later' });
   }
 });
