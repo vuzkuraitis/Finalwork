c675603129157de67e1f474202007d83de6704eb
Vidmantas Uzkuraitis <v.uzkuraitis@yahoo.com>
2022-07-29 20:03:34 +0300

    email fix

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -181,7 +181,6 @@ router.post('/reset-password', validation(resetPasswordSchema), async (req, res)
         auth: mailServerPassword,
         to: req.body.email,
         subject: 'NO-REPLY: New Password',
-        text: `It seems that you have requested for a new password. To change password you will need this code ${randomCode}`,
         html: `<h3> Dear ${data1[0].name}</h3>
         <p>It seems that you have requested for a new password. To change password you will need this code:<br><br> 
         <span style="color: #636dd1; font-size: 1.5rem;">${randomCode}<span></p>
